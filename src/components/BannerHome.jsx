bb42ed4e5bc1e29c957ce9518c6d1edbd8e0e988
kiet-trann <kiettuantranle@gmail.com>
2024-06-23 20:16:49 +0700

    update banner moi

--- src/components/BannerHome.jsx
+++ src/components/BannerHome.jsx
@@ -1,80 +1,51 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Slider from "react-slick";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
-function BannerHome(props) {
+function BannerHome() {
     const FirstSliderSettings = {
         fade: true,
         infinite: true,
         speed: 1000,
         dots: true,
-        // autoplay: true,
+        autoplay: true,
         arrows: true,
         prevArrow: <button type="button" class="slick-prev pull-left"><i class="pe-7s-angle-left"></i></button>,
         nextArrow: <button type="button" class="slick-next pull-right"><i class="pe-7s-angle-right"></i></button>,
 
     };
-  return (
-    <div>
-       <section className="slider-area">
+    return (
+        <div>
+            <section className="slider-area">
                 <div className="hero-slider-active slick-arrow-style slick-arrow-style_hero slick-dot-style">
                     <Slider {...FirstSliderSettings}>
                         <div className="hero-single-slide hero-overlay">
-                            <div className="hero-slider-item bg-img" style={{ backgroundImage: `url(/assets/img/slider/home2-slide1.jpg)` }}>
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            <div className="hero-slider-content slide-1">
-                                                <h2 className="slide-title">KHUYÊN TAI KIM CƯƠNG</h2>
-                                                <h4 className="slide-desc">Giá chỉ từ 1.999.000đ</h4>
-                                                <a href="shop.html" className="btn btn-hero">KHÁM PHÁ NGAY</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <Link to="/Nhan">
+                                <div className="hero-slider-item bg-img" style={{ backgroundImage: `url(/assets/img/slider/banner-1.jpg)` }} />
+                            </Link>
                         </div>
 
-
-
                         <div className="hero-single-slide hero-overlay">
-                            <div className="hero-slider-item bg-img" style={{ backgroundImage: `url(/assets/img/slider/home2-slide2.jpg)` }}>
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            <div className="hero-slider-content slide-2">
-                                                <h2 className="slide-title">BỘ SƯU TẬP NHẪN </h2>
-                                                <h4 className="slide-desc">Giá chỉ từ <important> 2.990.000đ</important></h4>
-                                                <a href="shop.html" className="btn btn-hero">KHÁM PHÁ NGAY</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div className="hero-slider-item bg-img" style={{ backgroundImage: `url(/assets/img/slider/banner-2.jpg)` }}>
+
                             </div>
                         </div>
-
-
-
                         <div className="hero-single-slide hero-overlay">
-                            <div className="hero-slider-item bg-img" style={{ backgroundImage: `url(/assets/img/slider/home1-slide1.jpg)` }}>
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            <div className="hero-slider-content slide-3">
-                                                <h2 className="slide-title">SẢN PHẨM MỚI</h2>
-                                                <h4 className="slide-desc">Trang sức, kim cương.</h4>
-                                                <a href="shop.html" className="btn btn-hero">KHÁM PHÁ NGAY</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div className="hero-slider-item bg-img" style={{ backgroundImage: `url(/assets/img/slider/banner-3.jpg)` }}>
+
+                            </div>
+                        </div>
+                        <div className="hero-single-slide hero-overlay">
+                            <div className="hero-slider-item bg-img" style={{ backgroundImage: `url(/assets/img/slider/banner-4.png)` }}>
+
                             </div>
                         </div>
                     </Slider>
                 </div>
             </section>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default BannerHome
