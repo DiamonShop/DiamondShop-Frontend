b996ff1c72cf7c38bcac311528515a6a9084f733
Hung1303 <hungdtse172297@fpt.edu.vn>
2024-07-11 07:52:20 +0700

    tao va luu bill trong local storage cua FE

--- src/pages/cart/Dat_hang_thanh_cong.jsx
+++ src/pages/cart/Dat_hang_thanh_cong.jsx
@@ -1,52 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Dat_hang_thanh_cong() {
   const [transactionId, setTransactionId] = useState('');
+  const [billId, setBillId] = useState('');
 
   useEffect(() => {
     setTransactionId(Math.floor(100000000 + Math.random() * 900000000));
+    const storedBillId = localStorage.getItem('billId');
+    setBillId(storedBillId);
+    localStorage.removeItem('billId'); // Optionally remove it if not needed anymore
   }, []);
+
   return (
     <div>
       <div className='box-dat-hang'>
-        <div class="container-dat-hang">
-          <div class="printer-top"></div>
+        <div className="container-dat-hang">
+          <div className="printer-top"></div>
 
-          <div class="paper-container">
-            <div class="printer-bottom"></div>
+          <div className="paper-container">
+            <div className="printer-bottom"></div>
 
-            <div class="paper">
-              <div class="main-contents">
-                <div class="success-icon">&#10004;</div>
-                <div class="success-title">
+            <div className="paper">
+              <div className="main-contents">
+                <div className="success-icon">&#10004;</div>
+                <div className="success-title">
                   Đặt hàng thành công
                 </div>
-                <div class="success-description">
+                <div className="success-description">
                   Cảm ơn bạn đã hoàn tất thanh toán! Bạn sẽ nhận được sản phẩm bạn đã mua trong thời gian sớm nhất.
                 </div>
-                <div class="order-details">
-                  <div class="order-number-label">Mã đơn hàng</div>
-                  <div class="order-number" >{transactionId}</div>
-                  <div class="complement">Thank You!</div>
+                <div className="order-details">
+                  <div className="order-number-label">Mã đơn hàng</div>
+                  <div className="order-number">{transactionId}</div>
+                  <div className="order-number-label">Mã hóa đơn</div>
+                  <div className="order-number">{billId || "Not available"}</div>
+                  <div className="complement">Thank You!</div>
                   <div className='btn-dat-hang-thanh-cong'>
-                    <Link to="/" class="btn-return-home">Tiếp tục mua hàng</Link>
+                    <Link to="/" className="btn-return-home">Tiếp tục mua hàng</Link>
                     <div>
-                      <Link to="/" class="btn-view-cart">Xem đơn hàng</Link>
+                      <Link to="/" className="btn-view-cart">Xem đơn hàng</Link>
                     </div>
                   </div>
                 </div>
 
               </div>
 
-              <div class="jagged-edge"></div>
+              <div className="jagged-edge"></div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Dat_hang_thanh_cong
+export default Dat_hang_thanh_cong;
