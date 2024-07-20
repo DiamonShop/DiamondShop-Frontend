79aae4170a1f4a69af5a44e5238418ebcef50ef3
Hung1303 <hungdtse172297@fpt.edu.vn>
2024-07-20 18:09:32 +0700

    update user's loyalpoint, update jewelry stock

--- src/pages/cart/Dat_hang_thanh_cong.jsx
+++ src/pages/cart/Dat_hang_thanh_cong.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { decodeToken } from '../../api/TokenAPI';
 import { handleGetLatestOrderByUserId, handleUpdateStatusToPending } from '../../api/OrderAPI';
-import { handleUpdateUserLoyalPoint} from '../../api/UpdateProfile';
+import { handleUpdateUserLoyalPoint, handleUpdateJewelryQuantity} from '../../api/UpdateProfile';
 
 function Dat_hang_thanh_cong() {
   const [order, setOrder] = useState([]);
@@ -13,6 +13,7 @@ function Dat_hang_thanh_cong() {
       const userId = decodeToken(token).sid;
       await handleUpdateStatusToPending(userId);
       await handleUpdateUserLoyalPoint(userId);
+      await handleUpdateJewelryQuantity(userId);
     }
   }
 
